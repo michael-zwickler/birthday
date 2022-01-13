2a363019c7f57ed4126ecb4ac1294369ac7b1e44
Michael Zwickler <zwickler.michael@googlemail.com>
2022-01-13 11:20:31 +0100

    29 Februar handled

--- lib/birthday_countdown.rb
+++ lib/birthday_countdown.rb
@@ -3,6 +3,13 @@ require 'date'
 class BirthdayCountdown
 
   def days_to_birthday(day, month)
+    
+    feb_day = 0
+    if day == 29 && month == 2
+      feb_day = 1
+      day = 28
+    end
+
     birthday_date = Date.new(Time.now.year, month, day)
     
     if birthday_date - Date.today < 0
@@ -10,7 +17,7 @@ class BirthdayCountdown
     end
     
     result = birthday_date - Date.today
-    return result.to_i
+    return result.to_i + feb_day
   end
 
 end
